a3242f6f47a4abc103f28d60b88fbce7e6f34608
Zak Ahmed <zakahmed2006@hotmail.com>
2025-10-27 17:23:44 +0000

    clean up ignored files

--- backend/src/routes/stripe.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createCheckoutSession } from "../controllers/stripe.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
-
-const router = express.Router();
-
-router.post("/create-checkout-session", protectRoute, createCheckoutSession);
-
-export default router;

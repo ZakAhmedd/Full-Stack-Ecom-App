505f8e9f0589130a494c3da16f1cb45a330804c0
Zak Ahmed <zakahmed2006@hotmail.com>
2025-10-29 04:35:31 +0000

    Final

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { loginAdmin } from "../controllers/admin.controller.js";
+import { adminOnly } from "../middleware/admin.middleware.js";
 
 const router = express.Router();
 
-router.post("/login", loginAdmin);
+router.post("/login", adminOnly, loginAdmin);
 
 export default router;
